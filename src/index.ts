d3bc44d7354e688b24e503dda193fc4be0e6a2fd
Sakib Rahman <seu.sakib@gmail.com>
2023-01-31 18:55:50 +0600

    project init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import express from "express";
+// dependencies
 import cors from "cors";
+import express from "express";
 
+// config
+const port = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.status(200).send("Welcome to the Random API");
+});
+
+app.listen(port, () => {
+  console.log("Listing to port:", port);
+});
