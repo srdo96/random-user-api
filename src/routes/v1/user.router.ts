ad5bf27175269c8d574cf2823cbc573bf8b254dc
Sakib Rahman <seu.sakib@gmail.com>
2023-02-01 01:04:29 +0600

    Implement rate limiter middleware for /user/all endpoint to limit repeated requests in 15 min

--- src/routes/v1/user.router.ts
+++ src/routes/v1/user.router.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import * as userController from "../../controllers/user.controller";
+import rateLimiter from "../../middleware/limiter";
 
 const route = express.Router();
-
 route.get("/random", userController.getRandomUser);
-route.get("/all", userController.getAllUser);
+route.get("/all", rateLimiter, userController.getAllUser);
 
 export default route;
