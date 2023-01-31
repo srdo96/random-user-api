46b859aa99170e6d32c2c3539362bd783e280ff8
Sakib Rahman <seu.sakib@gmail.com>
2023-02-01 00:32:50 +0600

    Implement getAllUser controller for /user/all endpoint to return all users form userData.json file

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express";
 import fs from "node:fs";
 
+const userDataPath = `${__dirname}/../../userData.json`;
+
 export const getRandomUser = async (_req: Request, res: Response) => {
   try {
     // read userData file
-    const rawData = await fs.promises.readFile(
-      `${__dirname}/../../userData.json`,
-      "utf-8"
-    );
-    const data: [] = JSON.parse(rawData);
+    const stringData = await fs.promises.readFile(userDataPath, "utf-8");
+    const data: [] = JSON.parse(stringData);
     const randomIndex = Math.floor(Math.random() * data.length);
     const randomUser = data[randomIndex];
     res.status(200).send(randomUser);
@@ -18,6 +17,20 @@ export const getRandomUser = async (_req: Request, res: Response) => {
       res.status(404).send("File Not Found");
     } else {
       // other error
+      res.status(500).send("Unexpected Error");
+    }
+  }
+};
+
+export const getAllUser = async (_req: Request, res: Response) => {
+  try {
+    const data: string = await fs.promises.readFile(userDataPath, "utf-8");
+    res.status(200).send(data);
+  } catch (error: unknown) {
+    // File Not Found
+    if ((error as NodeJS.ErrnoException).code === "ENOENT") {
+      res.status(404).send("File Not Found");
+    } else {
       res.status(500).send("Unexpected Error");
     }
   }
--- src/routes/v1/user.router.ts
+++ src/routes/v1/user.router.ts
@@ -4,5 +4,6 @@ import * as userController from "../../controllers/user.controller";
 const route = express.Router();
 
 route.get("/random", userController.getRandomUser);
+route.get("/all", userController.getAllUser);
 
 export default route;
